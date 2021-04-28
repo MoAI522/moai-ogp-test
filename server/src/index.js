f72860268c511ba05357a8fbff1f263d54f7c49f
MoAI <urotanre522@gmail.com>
2021-04-29 02:51:05 +0900

    fix port setting

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-var port = 3000;
+var port = process.env.PORT || 3000;
 var router = express_1.default.Router();
 router.use(express_1.default.static(__dirname + "/static"));
 router.get("*", function (_, res) { return res.sendFile(__dirname + "/static/index.html"); });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const router = express.Router();
 router.use(express.static(`${__dirname}/static`));
